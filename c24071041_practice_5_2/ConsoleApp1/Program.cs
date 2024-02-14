7ed6c21968d5577333e0b5835738825981e020a9
qazwsx351789 <112364187+qazwsx351789@users.noreply.github.com>
2024-02-14 23:49:20 +0800

    practice 3

---
+++ c24071041_practice_5_2/ConsoleApp1/Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        private static int[] RandomN(int num)
+        {
+            int[] arr;
+            arr = new int[num];
+            int ranNo;
+            Random ranObj = new Random();
+            for (int i = 0; i < num; i++)
+            {
+                ranNo = ranObj.Next(101);
+                arr[i] = ranNo;
+            }
+            Array.Sort(arr);
+            return (arr);
+        }
+
+        static void Main(string[] args)
+        {
+            int a, b, N;
+            string str1 = "";
+            Console.Write("Input N :");
+            N = int.Parse(Console.ReadLine());
+            int[] data = RandomN(N);
+            foreach (int num in data)
+                str1 += num.ToString() + " ";
+            Console.WriteLine("{0}",str1);
+
+            Console.Write("Input Start :");
+            a = int.Parse(Console.ReadLine());
+
+            Console.Write("Input End :");
+            b = int.Parse(Console.ReadLine());
+            int n = 0;
+            foreach(int num in data)
+            {
+                if (num>a && num<b)
+                {
+                    n += 1;
+                }
+            }
+            Console.Write("{0} numbers.",n);
+
+
+
+            Console.ReadKey();
+
+
+        }
+    }
+}
